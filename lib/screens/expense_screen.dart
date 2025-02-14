9c2694c56f4b6a8291759db5f6db66e74c33ca77
Morgane BINAZON <morganebinazon@gmail.com>
2025-02-14 11:32:18 +0100

    graphique des pages

--- lib/screens/expense_screen.dart
+++ lib/screens/expense_screen.dart
@@ -1,11 +1,38 @@
 import 'package:flutter/material.dart';
 
-class ExpenseScreen extends StatelessWidget {
+class ExpenseScreen extends StatefulWidget {
   @override
+  _ExpenseScreenState createState() => _ExpenseScreenState();
+}
+
+class _ExpenseScreenState extends State<ExpenseScreen> {
+  List<Map<String, dynamic>> _expenses = [];
+
+  void _addExpense() {
+    setState(() {
+      _expenses.add({"title": "Dépense ${_expenses.length + 1}", "amount": (10 + _expenses.length) * 100});
+    });
+  }
+ @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Dépenses")),
-      body: Center(child: Text("Suivi des dépenses ici")),
-    );
-  }
+      body: _expenses.isEmpty
+          ? Center(child: Text("Aucune dépense enregistrée"))
+          : ListView.builder(
+              itemCount: _expenses.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: Icon(Icons.money, color: Colors.green),
+                  title: Text(_expenses[index]["title"]),
+                  subtitle: Text("${_expenses[index]["amount"]} FCFA"),
+                );
+              },
+            ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _addExpense,
+        child: Icon(Icons.add),
+      ),
+    );
+  }
 }
